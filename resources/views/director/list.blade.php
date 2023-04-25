5c2632dd0c6d94b5613f7d0bb0f9eb534333ba49
Jean VonexPc <bikersprop@gmail.com>
2023-04-25 14:02:54 -0500

    Fix Module Directors list tutors

--- resources/views/director/list.blade.php
+++ resources/views/director/list.blade.php
@@ -1,7 +1,7 @@
 @foreach ($cycles as $cycle)
   <tr>
     <td class="text-secondary">{{ $cycle->codigo_final }}</td>
-    <td class="text-secondary">{{ $cycle->apellido_tutor }} {{ $cycle->apellido_tutor }}</td>
+    <td class="text-secondary">{{ $cycle->apellido_tutor }} {{ $cycle->nombre_tutor }}</td>
     <td>
       <div class="form-check form-switch d-flex justify-content-center position-relative">
         <div class="radio__loading position-absolute"></div>
