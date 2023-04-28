006767aaf918b9a2ee81a0f20f0cf51ede4d112d
Jean VonexPc <bikersprop@gmail.com>
2023-04-28 15:35:57 -0500

    test horary 13 remove

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -11,7 +11,7 @@ class Administrador extends Model {
   protected $fillable = ['email'];
   protected $table = 'administradores';
 
-  public const TIMESURVEYSTART = -237;
-  public const TIMESURVEYEND = 1;
+  public const TIMESURVEYSTART = 20;
+  public const TIMESURVEYEND = 10;
   public const SUPERADMINS = ["jcuadros@vonex.edu.pe", "soporte@vonex.edu.pe"];
 }
